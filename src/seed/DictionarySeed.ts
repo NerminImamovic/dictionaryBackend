0ece70ca5fc0b1ae4e4ec09b44b4d8408b083d18
NerminImamovic <nimamovic9@gmail.com>
2020-07-04 13:09:44 +0200

    remove log

--- src/seed/DictionarySeed.ts
+++ src/seed/DictionarySeed.ts
@@ -4,9 +4,7 @@ import { DictionaryService } from "../services/DictionaryService";
 function seedDictionary(): void {
 
     const dictionaryService: IDictionaryService = new DictionaryService();
-
-    console.log("Seeding Dictionary" + JSON.stringify(dictionaryService));
-
+    
     dictionaryService.CreateWord("a");
     dictionaryService.CreateWord("b");
 
